9ac3bc60447cdfddf0ca3f5c5dfc300821f1f657
Antonello <m3rcur14l@hotmail.com>
2015-07-11 19:53:50 +0200

    nfc external type integration

--- mobile/src/main/java/com/como/laps/byu/ScanActivity.java
+++ mobile/src/main/java/com/como/laps/byu/ScanActivity.java
@@ -1,17 +1,125 @@
 package com.como.laps.byu;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
 
 public class ScanActivity extends Activity {
+
+    private PendingIntent nfcPendingIntent;
+    private IntentFilter[] intentFiltersArray;
+    private NfcAdapter nfcAdpt;
+    private TextView tagsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        tagsView = (TextView) findViewById(R.id.tags);
+        Intent nfcIntent = new Intent(this, getClass());
+        nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
+
+        IntentFilter tagIntentFilter =
+                new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            tagIntentFilter.addDataType("text/plain");
+            intentFiltersArray = new IntentFilter[]{tagIntentFilter};
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        nfcAdpt = NfcAdapter.getDefaultAdapter(this);
+        // Check if the smartphone has NFC
+        if (nfcAdpt == null) {
+            Toast.makeText(this, "NFC not supported", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        // Check if NFC is enabled
+        if (!nfcAdpt.isEnabled()) {
+            Toast.makeText(this, "Enable NFC before using the app", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d("Nfc", "New intent");
+        try {
+            getTag(intent);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getTag(Intent i) throws UnsupportedEncodingException {
+        if (i == null)
+            return;
+
+        String type = i.getType();
+        String action = i.getAction();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Log.d("Nfc", "Action NDEF Found");
+            Parcelable[] parcs = i.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            // List record
+
+
+            for (Parcelable p : parcs) {
+                NdefMessage msg = (NdefMessage) p;
+                final int numRec = msg.getRecords().length;
+
+                NdefRecord[] records = msg.getRecords();
+                for (NdefRecord record : records) {
+                    String text = tagsView.getText().toString();
+                    byte[] payload = record.getPayload();
+                    String payloadText = new String(payload, "UTF-8");
+                    tagsView.setText(text.concat(payloadText));
+                }
+            }
+
+        }
+
+    }
+
+
+    private void handleIntent(Intent i) {
+        Log.d("NFC", "Intent [" + i + "]");
+        try {
+            getTag(i);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdpt.enableForegroundDispatch(
+                this,
+                nfcPendingIntent,
+                intentFiltersArray,
+                null);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nfcAdpt.disableForegroundDispatch(this);
     }
 
     @Override
